166b7041811d684821b1e4cc58b2b3f6648dc2c0
ckashby <clay.ashby@gmail.com>
2018-03-28 18:40:41 -0700

    reorganized routes.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+  self.abstract_class = true  # Tells Rails there is no table associated with this model.
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
+  root to: redirect('/ideas')
+
   get 'pages/info'
 
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: redirect('/ideas')
-
+  
 end
